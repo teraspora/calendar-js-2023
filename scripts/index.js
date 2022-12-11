71e4ba251cb780811a7179cd949e93017f4e2032
John Lynch <johnlynch.32768@gmail.com>
2022-12-11 16:43:37 +0000

    Initial commit for calendar-js-2023; based on calendar-js-2019.

--- scripts/index.js
+++ scripts/index.js
@@ -1,88 +1,36 @@
 const months = [
-	{
-		name: "January",
-		len: 31,
-		starts: 1
-	},
-	{
-		name: "February",
-		len: 28,
-		starts: 4
-	},
-	{
-		name: "March",
-		len: 31,
-		starts: 4 
-	},
-	{
-		name: "April",
-		len: 30,
-		starts: 0
-	},
-	{
-		name: "May",
-		len: 31,
-		starts: 2
-	},
-	{
-		name: "June",
-		len: 30,
-		starts: 5
-	},
-	{
-		name: "July",
-		len: 31,
-		starts: 0
-	},
-	{
-		name: "August",
-		len: 31,
-		starts: 3
-	},
-	{
-		name: "September",
-		len: 30,
-		starts: 6
-	},
-	{
-		name: "October",
-		len: 31,
-		starts: 1
-	},
-	{
-		name: "November",
-		len: 30,
-		starts: 4
-	},
-	{
-		name: "December",
-		len: 31,
-		starts: 6
-	}
+	{ name: 'January', len: 31, starts: 6 },
+	{ name: 'February', len: 28, starts: 2 },
+	{ name: 'March', len: 31, starts: 2 },
+	{ name: 'April', len: 30, starts: 5 },
+	{ name: 'May', len: 31, starts: 0 },
+	{ name: 'June', len: 30, starts: 3 },
+	{ name: 'July', len: 31, starts: 5 },
+	{ name: 'August', len: 31, starts: 1 },
+	{ name: 'September', len: 30, starts: 4 },
+	{ name: 'October', len: 31, starts: 6 },
+	{ name: 'November', len: 30, starts: 2 },
+	{ name: 'December', len: 31, starts: 4 }
 ];
 
-let monthNum = 9;
-let month = months[monthNum];
+const cols = ['#3dfcff', '#ffdcbd', '#b7ffff', '#cbe6a7', '#80c4ff', '#f4c8e7', '#acfcc5']
 
-document.getElementById("month-name").textContent = `${month.name} 2019`;
+const monthNum = 0;
+const month = months[monthNum];
+
+document.getElementById("month-name").textContent = `${month.name} 2023`;
 
 for (let dayNum = 1; dayNum <= month.len; dayNum++) {
-	let boxNum = dayNum + month.starts - 1;
-	let day = document.querySelectorAll(".day")[boxNum];
+	const boxNum = dayNum + month.starts - 1;
+	const dayOfWeekNum = boxNum % 7;
+	const day = document.getElementsByClassName('day')[boxNum];
 	day.textContent = dayNum;
-	let r = 255 - Math.floor(Math.random() * 96);
-	let g = 255 - Math.floor(Math.random() * 128);
-	let b = 255 - Math.floor(Math.random() * 96);
-	let dayOfWeekNum = boxNum % 7;
-	if (dayOfWeekNum == 5 || dayOfWeekNum == 6) {
-		[g, b] = [g-96, b-96];
-	}
-
-	day.style.backgroundColor = `rgb(${r}, ${g}, ${b})`;
+	day.style.backgroundColor = cols[dayOfWeekNum];
 }
 
-for (let boxNum = 0; boxNum < 35; boxNum++) {
-	let box = document.querySelectorAll(".day")[boxNum];
+
+for (let boxNum = 0; boxNum < 42; boxNum++) {
+	const box = document.querySelectorAll(".day")[boxNum];
 	if (box.textContent.trim() == "") {
 		box.style.borderStyle = "none";
 	}
